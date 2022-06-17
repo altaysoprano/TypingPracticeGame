c9140a48a9c2803ef3041ee85477ebcbff85f8eb
Hasan Cihad Altay <caltayzzz@gmail.com>
2022-06-17 14:43:17 +0300

    butona basınca klavye açılıyor

--- app/src/main/java/com/example/typingpractice/MainScreen.kt
+++ app/src/main/java/com/example/typingpractice/MainScreen.kt
@@ -3,21 +3,22 @@ package com.example.typingpractice
 import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.typingpractice.ScoreBoard
 import com.example.typingpractice.ui.MainScreenViewModel
-import kotlinx.coroutines.delay
 
 @ExperimentalComposeUiApi
 @Composable
@@ -25,6 +26,8 @@ fun MainScreen(
     viewModel: MainScreenViewModel = hiltViewModel(),
 ) {
     val number = viewModel.score.value
+    val kc = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
 
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column(modifier = Modifier.align(Alignment.Center)) {
@@ -33,8 +36,17 @@ fun MainScreen(
             Button(onClick = { viewModel.increaseScore(1) }) { Text(text = "Increase Number") }
             Spacer(modifier = Modifier.height(12.dp))
             Button(onClick = {
+                focusRequester.requestFocus()
+                kc?.show()
                 viewModel.startGame()
             }) { Text(text = "Start the game") }
+            OutlinedTextField(
+                modifier = Modifier
+                    .alpha(0f)
+                    .focusRequester(focusRequester),
+                value = "",
+                onValueChange = {}
+            )
         }
     }
 }

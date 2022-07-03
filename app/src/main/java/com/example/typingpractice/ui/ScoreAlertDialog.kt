640fe2d47fb5e3dbcf0d0acc346042b57e7b80f1
Hasan Cihad Altay <caltayzzz@gmail.com>
2022-07-03 16:15:21 +0300

    alertDialog done (2)

--- app/src/main/java/com/example/typingpractice/ui/ScoreAlertDialog.kt
+++ app/src/main/java/com/example/typingpractice/ui/ScoreAlertDialog.kt
@@ -1,11 +1,23 @@
 package com.example.typingpractice.ui
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ScoreAlertDialog(
@@ -17,14 +29,36 @@ fun ScoreAlertDialog(
     Log.d("Mesaj: ", "Score (AlertDialog): " + score.toString())
     if (dialogState) {
         AlertDialog(
-            onDismissRequest = { onDismiss()
+            onDismissRequest = {
+                onDismiss()
             },
-            title = { Text(text = "Your score: ", fontWeight = FontWeight.Bold)},
-            text = { Text(text = score.toString()) },
+            title = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "Your score: ", fontWeight = FontWeight.Bold, fontSize = 28.sp)
+                }
+            },
+            text = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(text = score.toString(), fontSize = 24.sp)
+                }
+            },
             confirmButton = {
-                TextButton(onClick = { onDismiss()
-                 }) {
-                    Text(text = "OK")
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    TextButton(
+                        onClick = { onDismiss() },
+                        border = BorderStroke(1.dp, color = MaterialTheme.colors.onPrimary)
+                    ) {
+                        Text(text = "OK")
+                    }
                 }
             }
         )
